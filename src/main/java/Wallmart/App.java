0b3ab8009bdd8fadcfb4403d32f10f19614d273a
Hari Prasad <Hari@devops.com>
2022-10-22 01:09:36 +0530

    files Moved

---
+++ src/main/java/Wallmart/App.java
@@ -0,0 +1,13 @@
+package Wallmart;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
